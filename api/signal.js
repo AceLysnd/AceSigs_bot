3de6166208e89ec9acc3ff271df8069aa7d992bf
Ilham Akbar Pradana <ilhamakbar.pradana@gmail.com>
2025-06-16 03:33:59 +0700

    Kucoin

--- api/signal.js
+++ api/signal.js
@@ -4,10 +4,10 @@ import axios from 'axios';
 export default async function handler(req, res) {
   const TELEGRAM_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
   const CHAT_ID = process.env.TELEGRAM_CHAT_ID;
-  const SYMBOL = 'BTC/USDT:USDT';
+  const SYMBOL = 'BTC/USDT';
 
   try {
-    const exchange = new ccxt.bybit();
+    const exchange = new ccxt.kucoin();
     const candles = await exchange.fetchOHLCV(SYMBOL, '5m', undefined, 100);
     const closes = candles.map(c => c[4]); // closing prices
 
